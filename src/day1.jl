029cf5757a4d8845b9f7c2444d6ce2a50885481c
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-12-01 06:05:31 +0100

    Day 1

--- src/day1.jl
+++ src/day1.jl
@@ -1,6 +1,36 @@
 
 function part1()
-    for l in eachline("input/day1/input")
+    elves = Int[]
 
+    elf = Int[]
+
+    for l in eachline("input/day1/input")
+        if length(l) == 0
+            push!(elves, sum(elf))
+            empty!(elf)
+        else
+            push!(elf, parse(Int, l))
+        end
     end
+
+    maximum(elves)
+end
+
+function part2()
+    elves = Int[]
+
+    elf = Int[]
+
+    for l in eachline("input/day1/input")
+        if length(l) == 0
+            push!(elves, sum(elf))
+            empty!(elf)
+        else
+            push!(elf, parse(Int, l))
+        end
+    end
+
+    sort!(elves)
+
+    sum(elves[end-2:end])
 end
