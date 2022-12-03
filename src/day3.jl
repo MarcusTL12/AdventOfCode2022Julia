9a4c6cae5784ec2ad0c13573a31b3524f6d8e94a
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-12-03 08:43:48 +0100

    Solution

---
+++ src/day3.jl
@@ -0,0 +1,39 @@
+
+function priority(c)
+    if islowercase(c)
+        c - 'a' + 1
+    else
+        c - 'A' + 27
+    end
+end
+
+function part1()
+    x = 0
+
+    for l in eachline("input/day3/input")
+        c, = intersect(
+            (@view l[1:length(l)÷2]),
+            (@view l[length(l)÷2+1:end])
+        )
+
+        x += priority(c)
+    end
+
+    x
+end
+
+function part2()
+    lines = collect(eachline("input/day3/input"))
+
+    x = 0
+
+    for (a, b, c) in eachcol(reshape(lines, 3, length(lines) ÷ 3))
+
+        s = intersect(a, b, c)
+
+        ch, = s
+        x += priority(ch)
+    end
+
+    x
+end
