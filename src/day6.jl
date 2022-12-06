899057c36fb0513cb27dbfb64fedb610793fe645
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-12-06 06:13:39 +0100

    Clean up

--- src/day6.jl
+++ src/day6.jl
@@ -1,35 +1,16 @@
 
-function part1()
-    s = ""
-
-    for l in eachline("input/day6/input")
-        s = l
-        break
-    end
-
-    x = 0
-
+function solve(s, n)
     for i in 1:length(s)
-        if length(Set(s[i:i+3])) == 4
-            @show s[i:i+3]
-            return i + 3
+        if length(Set(s[i:i+n-1])) == n
+            return i + n - 1
         end
     end
 end
 
+function part1()
+    solve(read("input/day6/input", String), 4)
+end
+
 function part2()
-    s = ""
-
-    for l in eachline("input/day6/input")
-        s = l
-        break
-    end
-
-    x = 0
-
-    for i in 1:length(s)
-        if length(Set(s[i:i+13])) == 14
-            return i + 13
-        end
-    end
+    solve(read("input/day6/input", String), 14)
 end
