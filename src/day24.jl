6d9089f2c3d45abdae7dbb064b62576c792731ee
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-12-24 07:05:20 +0100

    Saved some time with unsafe pruning

--- src/day24.jl
+++ src/day24.jl
@@ -19,7 +19,13 @@ function inside_board(x, y, w, h)
     (x, y) == (2, 1) || (x, y) == (w - 1, h) || 1 < x < w && 1 < y < h
 end
 
-function bfs(start, stop, t0, blizzards, w, h)
+function mandist(a, b)
+    sum(abs, a .- b)
+end
+
+function bfs(start, stop, t0, blizzards, w, h, tmax)
+    tmax += t0
+
     dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
     queue = [(start, t0)]
@@ -28,7 +34,8 @@ function bfs(start, stop, t0, blizzards, w, h)
     while !isempty(queue)
         (pos, t) = popfirst!(queue)
 
-        if !is_blizzard(blizzards, pos, t + 1, w, h) && (pos, t + 1) ∉ visited
+        if !is_blizzard(blizzards, pos, t + 1, w, h) &&
+           (pos, t + 1) ∉ visited && mandist(pos, stop) + t + 1 <= tmax
             push!(queue, (pos, t + 1))
             push!(visited, (pos, t + 1))
         end
@@ -42,7 +49,9 @@ function bfs(start, stop, t0, blizzards, w, h)
             end
 
             if !is_blizzard(blizzards, npos, nt, w, h) &&
-               (npos, nt) ∉ visited && inside_board(npos..., w, h)
+               (npos, nt) ∉ visited &&
+               inside_board(npos..., w, h) &&
+               mandist(npos, stop) + nt <= tmax
                 push!(queue, (npos, nt))
                 push!(visited, (npos, nt))
             end
@@ -78,7 +87,7 @@ function part1()
     w -= 1
     h = y - 1
 
-    bfs((2, 1), (w - 1, h), 0, blizzards, w, h)
+    bfs((2, 1), (w - 1, h), 0, blizzards, w, h, 320)
 end
 
 function part2()
@@ -112,9 +121,9 @@ function part2()
     start = (2, 1)
     goal = (w - 1, h)
 
-    time1 = bfs(start, goal, 0, blizzards, w, h)
+    time1 = bfs(start, goal, 0, blizzards, w, h, 320)
     @show time1
-    time2 = bfs(goal, start, time1, blizzards, w, h)
+    time2 = bfs(goal, start, time1, blizzards, w, h, 320)
     @show time2
-    bfs(start, goal, time2, blizzards, w, h)
+    bfs(start, goal, time2, blizzards, w, h, 320)
 end
